2d2010a6e3027a01df404636e95250c69c192d8a
TFG studios <thefallengamesstudio@gmail.com>
2021-01-24 01:00:09 +0200

    updated name and proper setup

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 from setuptools import setup
 
 setup(
-    name='splitlogger',
+    name='py_splitlogger',
     version='0.0.1',
-    description='Log to both file and console in real-time',
-    url='git@github.com:tfgstudios/py_splitlogger.git',
+    description="Log to both file and console in real-time. " +
+                "'Install by 'pip install git+https://github.com/tfgstudios/py_splitlogger.git#egg=py_splitlogger'",
+    url='https://github.com/tfgstudios/py_splitlogger.git',
     author='The Fallen Games',
     author_email='contact@thefallengames.com',
     license='unlicense',
-    packages=['splitlogger'],
+    packages=['py_splitlogger'],
     zip_safe=False
 )
