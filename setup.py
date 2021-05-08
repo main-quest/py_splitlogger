537df572c39d4c3a14108f2ea51d2954cd6b2ed4
Lucian Lazar <lucian@mainquest.ro>
2021-05-08 23:48:00 +0300

    updated repo url

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ setup(
     name='py_splitlogger',
     version='0.0.2',
     description="Log to both file and console in real-time. " +
-                "'Install by 'pip install --upgrade git+https://github.com/tfgstudios/py_splitlogger.git#egg=py_splitlogger'",
-    url='https://github.com/tfgstudios/py_splitlogger.git',
+                "'Install by 'pip install --upgrade git+https://github.com/main-quest/py_splitlogger.git#egg=py_splitlogger'",
+    url='https://github.com/main-quest/py_splitlogger.git',
     author='The Fallen Games',
     author_email='contact@thefallengames.com',
     license='unlicense',
